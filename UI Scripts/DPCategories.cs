417851d4be4bd4614dcbc626539e5be39e1c1100
HabibulMehdi <70104165+HabibulMehdi@users.noreply.github.com>
2020-08-23 23:16:40 +0500

    DP Learner
    40% Implementation.

---
+++ UI Scripts/DPCategories.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DPCategories : MonoBehaviour
+{
+    public void CreationalPatterns()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StructuralPatterns()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BehavioralPatterns()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ UI Scripts/Options.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    // reference to audio mixer
+    public AudioMixer audioMixer;
+    // reference to dropdown
+    public Dropdown resolutionDropdown;
+     
+    // setting resolution
+    Resolution[] resolutions;
+     void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> optionss = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            optionss.Add(option);
+            
+            if(resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(optionss);
+        resolutionDropdown.value = currentResolutionIndex;
+        // updating the drop down
+        resolutionDropdown.RefreshShownValue();
+
+    }
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+}
