6996487f4bcd24bc35d558da3ea8e274bfbf7e2e
HabibulMehdi <70104165+HabibulMehdi@users.noreply.github.com>
2020-10-26 21:54:20 +0500

    Add files via upload

---
+++ TriggerUIShow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class TriggerUIShow : MonoBehaviour
+{
+    public GameObject uiobject;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiobject.SetActive(false);
+    }
+    void OnTriggerEnter(Collider Player)
+    {
+        uiobject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider Player)
+    {
+        uiobject.SetActive(false);
+    }
+}
