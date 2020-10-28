032a3100703d218203d2372f10035fbaf76f545a
HabibulMehdi <70104165+HabibulMehdi@users.noreply.github.com>
2020-10-28 19:31:28 +0500

    Add files via upload

---
+++ CountdownTimer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    string currentTime;
+    float seconds = 0f;
+    float minutes = 1f;
+    float count = 0f;
+
+    [SerializeField] Text countdownText;
+     void Start()
+    {
+        count = minutes * 60;
+        minutes--;
+        currentTime = minutes.ToString() + ":" + seconds.ToString();
+        seconds = 60;
+    }
+
+     void Update()
+    {
+        count -= 1 * Time.deltaTime;
+        seconds -= 1 * Time.deltaTime;
+
+        if(seconds <= 0 && minutes != 0)
+        {
+            seconds = 60;
+            minutes -= 1;
+        }
+        currentTime = minutes.ToString() + ":" + seconds.ToString();
+        // print(currentTime);
+       
+        countdownText.text = currentTime;
+       
+        if (count <= 0)
+        {
+            currentTime = "0";
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        }
+    }
+}
