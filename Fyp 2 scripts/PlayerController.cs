8d44eeaafcbaffe55a20ca72d7317291e9da5442
HabibulMehdi <70104165+HabibulMehdi@users.noreply.github.com>
2020-12-22 06:26:07 +0500

    Add files via upload

---
+++ Fyp 2 scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject aj, james, kate, louise, ajCamera, kateCamera, louiseCamera, jamesCamera;
+    
+    
+    public void Start()
+    {
+        string character_Name = PlayerPrefs.GetString("character");
+
+        if (string.Equals(character_Name, "aj") == true)
+        {
+            aj.SetActive(true);
+            ajCamera.SetActive(true);
+            Minimap.player = aj.transform; 
+        }
+        else if (string.Equals(character_Name, "james") == true)
+        {
+            james.SetActive(true);
+            jamesCamera.SetActive(true);
+            Minimap.player = james.transform;
+        }
+        else if (string.Equals(character_Name, "kate") == true)
+        {
+            kate.SetActive(true);
+            kateCamera.SetActive(true);
+            Minimap.player = kate.transform;
+        }
+        else
+        {
+            louise.SetActive(true);
+            louiseCamera.SetActive(true);
+            Minimap.player = louise.transform;
+        }
+    }
+
+}
---
+++ Fyp 2 scripts/SelectAvatar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectAvatar : MonoBehaviour
+{
+    public Text characterName;
+    public Button aj, kate, james, louise;
+    public void aj_Button()
+    {
+        characterName.text = "aj";
+        PlayerPrefs.SetString("character",characterName.text);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void kate_Button()
+    {
+        characterName.text = "kate";
+        PlayerPrefs.SetString("character", characterName.text);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void louise_Button()
+    {
+        characterName.text = "louise";
+        PlayerPrefs.SetString("character", characterName.text);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void james_Button()
+    {
+        characterName.text = "james";
+        PlayerPrefs.SetString("character", characterName.text);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
