972db4f8d173115b662873d91e213c6ca027b201
HabibulMehdi <70104165+HabibulMehdi@users.noreply.github.com>
2020-12-22 06:29:28 +0500

    Add files via upload

---
+++ Fyp 2 scripts/BackScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackScript : MonoBehaviour
+{
+    public Button btn, btn1, btn2, btn3, btn4, btn5, btn6, btn7;
+    public GameObject back;
+    public Button done;
+
+    public void goback()
+    {
+        back.SetActive(false);
+        
+        if(!btn.gameObject.activeSelf && !btn3.gameObject.activeSelf && !btn4.gameObject.activeSelf 
+            && !btn6.gameObject.activeSelf && (!btn1.gameObject.activeSelf || btn1.interactable == false)
+            && (!btn2.gameObject.activeSelf || btn2.interactable == false) 
+            && (!btn5.gameObject.activeSelf || btn5.interactable == false) 
+            && (!btn.gameObject.activeSelf || btn7.interactable == false))
+        {
+            done.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Fyp 2 scripts/doneScript.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class doneScript : MonoBehaviour
+{
+    public GameObject resultCanvas;
+    public GameObject first_canvas;
+    public Button done;
+    public Button wrong1, wrong2;
+    public Text scoreText;
+    const string publicCode = "5fd7c239eb36fe27143bbef1";
+    const string webURL = "http://dreamlo.com/lb/";
+    Highscore[] highscoresList;
+    int len;
+    string _username;
+    int _score;
+
+    public void Start()
+    {
+        StartCoroutine("DownloadHighscoresFromDatabase");
+    }
+    public void showCanvas()
+    {
+        first_canvas.gameObject.SetActive(false);
+        resultCanvas.gameObject.SetActive(true);
+        _username = PlayerPrefs.GetString("Username");
+        
+        for(int z = 0; z < len; z++)
+        {
+            if (highscoresList[z].username == _username)
+            {
+                _score = highscoresList[z].score;
+                break;
+            }
+                
+        }
+
+        if (wrong1.interactable == false && wrong2.interactable == false)
+        {
+            scoreText.text = "10/10";
+            _score = _score + 10;
+            Highscores.AddNewHighscore(_username, _score);
+        }
+        else if (wrong1.interactable == false || wrong2.interactable == false)
+        {
+            scoreText.text = "5/10";
+            _score = _score + 5;
+            Highscores.AddNewHighscore(_username, _score);
+        }
+            
+        else
+        {
+            scoreText.text = "0/10";
+            _score = _score + 0;
+            Highscores.AddNewHighscore(_username, _score);
+        }
+        
+    }
+
+   /* public void DownloadHighscores()
+    {
+        DownloadHighscoresFromDatabase();
+    }
+*/
+    IEnumerator DownloadHighscoresFromDatabase()
+    {
+        WWW www = new WWW(webURL + publicCode + "/pipe/");
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            FormatHighscores(www.text);
+        }
+        else
+        {
+            print("Error Downloading: " + www.error);
+        }
+    }
+
+    void FormatHighscores(string textStream)
+    {
+        string[] entries = textStream.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        highscoresList = new Highscore[entries.Length];
+        len = entries.Length;
+        for (int i = 0; i < entries.Length; i++)
+        {
+            string[] entryInfo = entries[i].Split(new char[] { '|' });
+            string username = entryInfo[0];
+            int score = int.Parse(entryInfo[1]);
+            highscoresList[i] = new Highscore(username, score);
+            //Debug.Log(highscoresList[i].username + ": " + highscoresList[i].score);
+        }
+
+    }
+}
