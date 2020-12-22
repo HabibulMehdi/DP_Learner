b686356722e7c8613d03cc38a88cab5db747caa7
HabibulMehdi <70104165+HabibulMehdi@users.noreply.github.com>
2020-12-22 06:01:20 +0500

    Add files via upload

---
+++ 100% scripts/OneCutSceneSequence.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// for one Camera
+public class OneCutSceneSequence : MonoBehaviour
+{
+    // Animation Camera
+    public GameObject Cam1;
+    // Initializing the UI components
+    public GameObject dialogBox;
+    public Canvas mainCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TheSequence());
+    }
+
+    IEnumerator TheSequence()
+    {
+        yield return new WaitForSeconds(4);
+        // making the camera off
+        Cam1.SetActive(false);
+        dialogBox.SetActive(true);
+        mainCanvas.gameObject.SetActive(true);
+    }
+
+}
