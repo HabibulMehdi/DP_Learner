7a813173e6ab9c8896f53314b85ca9fc53a9ebc8
HabibulMehdi <70104165+HabibulMehdi@users.noreply.github.com>
2020-12-22 06:24:19 +0500

    Add files via upload

---
+++ Fyp 2 scripts/EvaluationIndexScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EvaluationIndexScript : MonoBehaviour
+{
+   public void abstract_Factory()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
